5bc27ebfd9a24f0ecaaba2cf9a0c0718c59e415f
Vitalii Bodnarchuk <vitaliy.bodnarchuk@headworks.com.ua>
2022-10-20 18:12:55 +0300

    added landing page bg header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,6 +3,9 @@ import "./Header.scss";
 import "../../index.scss";
 import RectanglesLeft from "../../assets/bg/rectangles-left.png";
 import RectanglesRight from "../../assets/bg/rectangles-right.png";
+import Cubes from "../../assets/icons/cubes.svg";
+import Roullete from "../../assets/icons/roulette.svg";
+import Cards from "../../assets/icons/cards.svg";
 
 const Header: FC = () => {
   return (
@@ -11,28 +14,40 @@ const Header: FC = () => {
       <img src={RectanglesRight} alt="" className="rectangles__right" />
       <div className="header">
         <ul className="header__list">
-          <li>
-            <a href="">XEN Lotto</a>
-          </li>
-          <li className="header__btn">
-            <a href="">Roulette</a>
-          </li>
-          <li>
-            <a href="">Poker</a>
-          </li>
-          <li>
-            <a href="">BurnXEN.io</a>
-          </li>
-          <li>
-            <a href="">Connect Wallet</a>
-          </li>
-          <li>
-            <a href="">
-              000,000,00 <span>XEN</span>
+          <li className="header__btn-lotto">
+            <a href="lotto-page" className="header__link">
+              <img src={Cubes} alt="" className="header__icon" /> XEN Lotto
             </a>
           </li>
-          <li>
-            <a href="">
+          <li className="header__btn">
+            <a href="" className="header__link">
+              <img src={Roullete} alt="" className="header__icon" />
+              Roulette
+            </a>
+          </li>
+          <li className="header__btn">
+            <a href="" className="header__link">
+              <img src={Cards} alt="" className="header__icon" />
+              Poker
+            </a>
+          </li>
+          <li className="header__btn-center">
+            <a href="/" className="header__link">
+              BurnXEN.io
+            </a>
+          </li>
+          <li className="header__btn">
+            <a href="" className="header__link">
+              Connect Wallet
+            </a>
+          </li>
+          <li className="header__btn">
+            <a href="" className="header__link">
+              000,000,00 <span className="header__span">XEN</span>
+            </a>
+          </li>
+          <li className="header__btn">
+            <a href="" className="header__link">
               Ethereum<span></span>
             </a>
           </li>
--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,78 +1,65 @@
 import React, { FC } from "react";
+import Header from "../../components/header/Header";
 import "../../index.scss";
 import "./LandingPage.scss";
+import MetaMask from "../../assets/icons/metamask.svg";
+import Cubes from "../../assets//img/landing/cubes.png";
 
 const LandingPage: FC = () => {
   return (
     <div className="background">
-      <div className="landing">
-        <div>
-          <h1 className="landing__title">BurnXEN.io</h1>
-          <ul>
-            <li>
-              <a href="/lotto-page">XEN Lotto</a>{" "}
-            </li>
-            <li>
-              <a href="/">
-                Roulette<span>Coming soon</span>
-              </a>
-            </li>
-            <li>
-              <a href="">
-                Poker<span>Coming soon</span>
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="landing__block">
-          <div className="landing__top">
-            <div className="landing__top-block">
-              <p>To get started</p>
-              <button className="landing__btn">Login with MetaMask</button>
-            </div>
-          </div>
-          <div className="landing__main">
-            <div className="landing__main-block">
-              <p>
-                389,135,105<span>XEN</span>
-              </p>
-              <div>
-                <p>Total Payout Today</p>
-              </div>
-            </div>
-            <div className="landing__main-block">
-              <p>
-                423,846,268<span>XEN</span>
-              </p>
-              <div>
-                <p>Total Payout All Time</p>
-              </div>
-            </div>
-            <div className="landing__main-block">
-              <p>
-                11,901,431<span>XEN</span>
-              </p>
-              <div>
-                <p>Burnt today</p>
-              </div>
-            </div>
-            <div className="landing__main-block">
-              <p>
-                80,953,235<span>XEN</span>
-              </p>
-              <div>
-                <p> Burnt All Time</p>
-              </div>
-            </div>
-          </div>
-          <div className="landing__main-about">
-            <div className="landing__main-about-block">
-              <p>What are we about!</p>
-            </div>
+      <Header />
+      <div className="landing__block">
+        <div className="landing__top">
+          <div className="landing__top-block">
+            <p className="landing__title">To get started</p>
+            <button className="landing__btn">
+              <img src={MetaMask} alt="" /> Login with MetaMask
+            </button>
           </div>
         </div>
-        <div>
-          <p>Eherium dropdown</p>
+        <div className="landing__main">
+          <div className="landing__main-block landing__main-block-left">
+            <p>
+              389,135,105<span>XEN</span>
+            </p>
+            <div>
+              <p>Total Payout Today</p>
+            </div>
+          </div>
+          <div className="landing__main-block landing__main-block-right">
+            <p>
+              423,846,268<span>XEN</span>
+            </p>
+            <div>
+              <p>Total Payout All Time</p>
+            </div>
+          </div>
+          <div className="landing__main-block landing__main-block-down-left">
+            <p>
+              11,901,431<span>XEN</span>
+            </p>
+            <div>
+              <p>Burnt today</p>
+            </div>
+          </div>
+          <div className="landing__main-block landing__main-block-down-right">
+            <p>
+              80,953,235<span>XEN</span>
+            </p>
+            <div>
+              <p> Burnt All Time</p>
+            </div>
+          </div>
+          <div className="landing__main-block-romb ">
+            <img src={Cubes} alt="" />
+            <p>XEN Lotto</p>
+          </div>
+        </div>
+        <div className="landing__main-about">
+          <div className="landing__main-about-block">
+            <p>What are we about!</p>
+          </div>
         </div>
       </div>
     </div>
