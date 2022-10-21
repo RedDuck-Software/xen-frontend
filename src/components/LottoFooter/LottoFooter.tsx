1de764114b30ffdb45b9200c36c8df74dd915ee9
Vitalii Bodnarchuk <vitaliy.bodnarchuk@headworks.com.ua>
2022-10-21 15:27:58 +0300

    added stats and winners to lotto-page

---
+++ src/components/LottoFooter/LottoFooter.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import LineTop from "../../assets/img/lotto/line-top.png";
+import LineBottom from "../../assets/img/lotto/line-bottom.png";
+import "../../index.scss";
+
+const LottoFooter = () => {
+  return (
+    <div className="lotto__footer">
+      <div className="lotto__footer-block">
+        <img src={LineTop} alt="" className="lotto__footer-line lotto__footer-line-top" />
+        <div className="landing__block-text">
+          <p className="landing__block-title">Next Lotto Prize</p>
+          <p className="landing__block-numbers">
+            18,567,000<span className="landing__block-span">XEN</span>
+          </p>
+        </div>
+        <img src={LineTop} alt="" className="lotto__footer-line lotto__footer-line-center" />
+        <div className="landing__block-text">
+          <p className="landing__block-title">Next Lotto Burn</p>
+          <p className="landing__block-numbers">
+            18,567,000<span className="landing__block-span">XEN</span>
+          </p>
+        </div>
+        <img src={LineBottom} alt="" className="lotto__footer-line lotto__footer-line-bottom" />
+      </div>
+      <div className="lotto__footer-block">
+        <img src={LineTop} alt="" className="lotto__footer-line lotto__footer-line-top" />
+        <div className="landing__block-text">
+          <p className="landing__block-title">Lotto Prize All Time</p>
+          <p className="landing__block-numbers">
+            1,108,567,000<span className="landing__block-span">XEN</span>
+          </p>
+        </div>
+        <img src={LineTop} alt="" className="lotto__footer-line lotto__footer-line-center" />
+        <div className="landing__block-text">
+          <p className="landing__block-title">Lotto Burn All Time</p>
+          <p className="landing__block-numbers">
+            18,567,000<span className="landing__block-span">XEN</span>
+          </p>
+        </div>
+        <img src={LineBottom} alt="" className="lotto__footer-line lotto__footer-line-bottom" />
+      </div>
+      <div className="lotto__footer-block">
+        <img src={LineTop} alt="" className="lotto__footer-line lotto__footer-line-top" />
+        <div className="landing__block-text">
+          <p className="landing__block-title">Total Draw All Time</p>
+          <p className="landing__block-numbers">
+            2574<span className="landing__block-span">XEN Draws</span>
+          </p>
+        </div>
+        <img src={LineTop} alt="" className="lotto__footer-line lotto__footer-line-center" />
+        <div className="landing__block-text">
+          <p className="landing__block-title">Total Winner All Time</p>
+          <p className="landing__block-numbers">
+            567<span className="landing__block-span">Players</span>
+          </p>
+        </div>
+        <img src={LineBottom} alt="" className="lotto__footer-line lotto__footer-line-bottom" />
+      </div>
+    </div>
+  );
+};
+
+export default LottoFooter;
--- src/pages/LottoPage/LottoPage.tsx
+++ src/pages/LottoPage/LottoPage.tsx
@@ -6,118 +6,147 @@ import LineBottom from "../../assets/img/lotto/line-bottom.png";
 import "../../index.scss";
 import "./LottoPage.scss";
 import { useWeb3React } from "@web3-react/core";
-
+import LottoFooter from "../../components/LottoFooter/LottoFooter";
+import AvatarSix from "../../assets/img/lotto/avatar/avatar-6.svg";
+import AvatarSeven from "../../assets/img/lotto/avatar/avatar-7.svg";
+import AvatarEight from "../../assets/img/lotto/avatar/avatar-8.svg";
+import AvatarNine from "../../assets/img/lotto/avatar/avatar-9.svg";
+import AvatarTen from "../../assets/img/lotto/avatar/avatar-10.svg";
+import AvatarEleven from "../../assets/img/lotto/avatar/avatar-11.svg";
+import AvatarTwelve from "../../assets/img/lotto/avatar/avatar-12.svg";
+import AvatarThirteen from "../../assets/img/lotto/avatar/avatar-13.svg";
+import Value from "../../assets/img/lotto/value.svg";
+import Share from "../../assets/img/lotto/share.svg";
+import LineStats from "../../assets/img/lotto/line-stats.png";
+import Crown from "../../assets/img/lotto/crown.svg";
 const LottoPage: FC = () => {
   const { account, connector, activate } = useWeb3React();
 
-  console.log('connector',connector)
+  console.log("connector", connector);
   return (
     <div className="wrapper wrapper-lotto">
       <Header />
       <div className="lotto">
         <div className="lotto-row">
           <Participants />
-          <div className="lotto-timer">
-            <p>NEXT DRAW IN</p>
-            <p>HOW MUCH XEN YOU WANT TO TO ENTER INTO DRAW </p>
-            <div className="lotto__block-scroll">
-              <div className="lotto__block-text">
-                <p>20,000</p>
-                <p>XEN</p>
-                <p>Enter draw</p>
-              </div>
-              <div className="lotto__block-subtext">
-                <p>current chance to win 1.37%</p>
-              </div>
-            </div>
-            <div className="lotto__bet">
-              <p>AutoBet is Off</p>
-              <p>Enable to enter new rounds automatically</p>
+          <div className="lotto__timer">
+            <div className="lotto__timer-bg">
+              <p>NEXT DRAW IN</p>
+              <p>00:58:23</p>
+              <p>Lotto Prize</p>
+              <p>
+                🔥18,465,657 <span>XEN</span>
+              </p>
             </div>
           </div>
           <div>
-            <div className="lotto__prize">
-              <p>Next Lotto Prize</p>
-              <p>18,567,000</p>
-            </div>
             <div className="lotto__stats">
-              <p>My Stats</p>
+              <img
+                src={LineTop}
+                alt=""
+                className="lotto__participants-line lotto__participants-line-top"
+              />
+              <div className="lotto__stats-my">
+                <div className="lotto__stats-my__header">
+                  <img src={Share} alt="" className="lotto__stats-my__header-img" />
+                  <p className="lotto__stats-my__header-title">My Stats</p>
+                </div>
+                <div className="lotto__stats-my__block">
+                  <p className="lotto__stats-my__block-title">My Entry</p>
+                  <p className="lotto__stats-my__number">
+                    20,000 <span className="lotto__stats-my__block-span">XEN</span>
+                  </p>
+                </div>
+                <img src={LineStats} alt="" className="lotto__stats-my__line" />
+                <div className="lotto__stats-my__block">
+                  <p className="lotto__stats-my__block-title">Chances of Winning</p>
+                  <p className="lotto__stats-my__block-number">5.37%</p>
+                </div>
+                <img src={LineStats} alt="" className="lotto__stats-my__line" />
+                <div className="lotto__stats-my__block">
+                  <p className="lotto__stats-my__block-title">Total winnings to date</p>
+                  <p className="lotto__stats-my__block-numbers">
+                    125,567,000 <span className="lotto__stats-my__block-span">XEN</span>
+                  </p>
+                </div>
+                <img
+                  src={LineBottom}
+                  alt=""
+                  className="lotto__participants-line lotto__stats-line__bottom"
+                />
+              </div>
             </div>
-            <div className="lotto__entry">
-              <p>My Entry</p>
-              <p>
-                20,000<span>XEN</span>
-              </p>
+            <div className="lotto__winners">
+              <img
+                src={LineTop}
+                alt=""
+                className="lotto__participants-line lotto__participants-line-top"
+              />
+              <div className="lotto__winners__header">
+                {" "}
+                <img src={Crown} alt="" className="lotto__participants-img" />
+                <p className="lotto__participants-title">Previou Winners</p>
+              </div>
+              <div>
+                <div className="lotto__participants-draw">
+                  <img src={AvatarSix} alt="" />
+                  <p className="lotto__participants-draw-text">Relly Blue</p>
+                  <img src={Value} alt="" />
+                  <p className="lotto__participants-draw-text">234.55 xen</p>
+                </div>
+                <div className="lotto__participants-draw">
+                  <img src={AvatarSeven} alt="" />
+                  <p className="lotto__participants-draw-text">Tim Ckil</p>
+                  <img src={Value} alt="" />
+                  <p className="lotto__participants-draw-text">199.45 xen</p>
+                </div>
+                <div className="lotto__participants-draw">
+                  <img src={AvatarEight} alt="" />
+                  <p className="lotto__participants-draw-text">Flip Trox</p>
+                  <img src={Value} alt="" />
+                  <p className="lotto__participants-draw-text">186.32 xen</p>
+                </div>
+                <div className="lotto__participants-draw">
+                  <img src={AvatarNine} alt="" />
+                  <p className="lotto__participants-draw-text">Sam Dux</p>
+                  <img src={Value} alt="" />
+                  <p className="lotto__participants-draw-text">167.30 xen</p>
+                </div>
+                <div className="lotto__participants-draw">
+                  <img src={AvatarTen} alt="" />
+                  <p className="lotto__participants-draw-text">Quentine Liv</p>
+                  <img src={Value} alt="" />
+                  <p className="lotto__participants-draw-text">102.12 xen</p>
+                </div>
+                <div className="lotto__participants-draw">
+                  <img src={AvatarEleven} alt="" />
+                  <p className="lotto__participants-draw-text">Yuval Hip</p>
+                  <img src={Value} alt="" />
+                  <p className="lotto__participants-draw-text">85.12 xen</p>
+                </div>
+                <div className="lotto__participants-draw">
+                  <img src={AvatarTwelve} alt="" />
+                  <p className="lotto__participants-draw-text">Maxxy Lip</p>
+                  <img src={Value} alt="" />
+                  <p className="lotto__participants-draw-text">72.21 xen</p>
+                </div>
+                <div className="lotto__participants-draw">
+                  <img src={AvatarThirteen} alt="" />
+                  <p className="lotto__participants-draw-text">Tim Dexter</p>
+                  <img src={Value} alt="" />
+                  <p className="lotto__participants-draw-text">65.77 xen</p>
+                </div>
+              </div>
+              <img
+                src={LineTop}
+                alt=""
+                className="lotto__participants-line lotto__participants-line-top"
+              />
             </div>
-            <div className="lotto__chance">
-              <p>My Current Chance Of Winning</p>
-              <p>1.37%</p>
-            </div>
-            <div className="lotto__winning">
-              <p>My Total Winnings To Date</p>
-              <p>
-                125,567,000<span>XEN</span>
-              </p>
-            </div>
-          </div>
-          <div>
-            <p>Previous Winners</p>
           </div>
         </div>
       </div>
-      <div className="lotto__footer">
-        <div className="lotto__footer-block">
-          <img src={LineTop} alt="" className="lotto__footer-line lotto__footer-line-top" />
-          <div className="landing__block-text">
-            <p className="landing__block-title">Next Lotto Prize</p>
-            <p className="landing__block-numbers">
-              18,567,000<span className="landing__block-span">XEN</span>
-            </p>
-          </div>
-          <img src={LineTop} alt="" className="lotto__footer-line lotto__footer-line-center" />
-          <div className="landing__block-text">
-            <p className="landing__block-title">Next Lotto Burn</p>
-            <p className="landing__block-numbers">
-              18,567,000<span className="landing__block-span">XEN</span>
-            </p>
-          </div>
-          <img src={LineBottom} alt="" className="lotto__footer-line lotto__footer-line-bottom" />
-        </div>
-        <div className="lotto__footer-block">
-          <img src={LineTop} alt="" className="lotto__footer-line lotto__footer-line-top" />
-          <div className="landing__block-text">
-            <p className="landing__block-title">Lotto Prize All Time</p>
-            <p className="landing__block-numbers">
-              1,108,567,000<span className="landing__block-span">XEN</span>
-            </p>
-          </div>
-          <img src={LineTop} alt="" className="lotto__footer-line lotto__footer-line-center" />
-          <div className="landing__block-text">
-            <p className="landing__block-title">Lotto Burn All Time</p>
-            <p className="landing__block-numbers">
-              18,567,000<span className="landing__block-span">XEN</span>
-            </p>
-          </div>
-          <img src={LineBottom} alt="" className="lotto__footer-line lotto__footer-line-bottom" />
-        </div>
-        <div className="lotto__footer-block">
-          <img src={LineTop} alt="" className="lotto__footer-line lotto__footer-line-top" />
-          <div className="landing__block-text">
-            <p className="landing__block-title">Total Draw All Time</p>
-            <p className="landing__block-numbers">
-              2574<span className="landing__block-span">XEN Draws</span>
-            </p>
-          </div>
-          <img src={LineTop} alt="" className="lotto__footer-line lotto__footer-line-center" />
-          <div className="landing__block-text">
-            <p className="landing__block-title">Total Winner All Time</p>
-            <p className="landing__block-numbers">
-              567<span className="landing__block-span">Players</span>
-            </p>
-          </div>
-          <img src={LineBottom} alt="" className="lotto__footer-line lotto__footer-line-bottom" />
-        </div>
-      </div>
+      <LottoFooter />
     </div>
   );
 };
