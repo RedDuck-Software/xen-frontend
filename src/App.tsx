d3aeb170d666bad98a115a2e608d97ab8458b0b1
Andrii <77144111+AndriiShvaika@users.noreply.github.com>
2022-10-26 16:32:36 +0300

    added metamask provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,61 @@
-import React, { FC, useContext, useEffect } from "react";
-import { Route, Routes, useNavigate } from "react-router-dom";
-import { useWeb3React, Web3ReactProvider } from "@web3-react/core";
-import { injected } from "./helpers/connectors";
-import { Web3Provider } from "@ethersproject/providers";
+import React, { FC, useContext, useEffect } from 'react'
+import { Route, Routes, useNavigate } from 'react-router-dom'
+import { useWeb3React, Web3ReactProvider } from '@web3-react/core'
+import { injected } from './helpers/connectors'
+import { Web3Provider } from '@ethersproject/providers'
 
-import Main from "./pages/MainPage/Main";
-import DepositPage from "./pages/DepositPage/DepositPage";
-import LandingPage from "./pages/LandingPage/LandingPage";
-import LottoPage from "./pages/LottoPage/LottoPage";
-import AccountPage from "./pages/AccountPage/AccountPage";
+import Main from './pages/MainPage/Main'
+import DepositPage from './pages/DepositPage/DepositPage'
+import LandingPage from './pages/LandingPage/LandingPage'
+import LottoPage from './pages/LottoPage/LottoPage'
+import AccountPage from './pages/AccountPage/AccountPage'
 
-import { Context } from "./Context";
+import { Context } from './Context'
 
-import "./index.scss";
+import './index.scss'
+import MetamaskProvider from './components/MetamaskProvider/MetamaskProvider'
 
 const App: FC = () => {
   function getLibrary(provider: any) {
     // @ts-ignore
-    return new Web3Provider(provider);
+    return new Web3Provider(provider)
   }
-  const { loggedIn, setLoggedIn, signer, setSigner } = useContext<any>(Context);
-  const { account, connector, activate } = useWeb3React();
-  const navigate = useNavigate();
+  const { loggedIn, setLoggedIn, signer, setSigner } = useContext<any>(Context)
+  const { connector } = useWeb3React()
+  const navigate = useNavigate()
 
   useEffect(() => {
-    console.log('here')
     if (!connector) {
-      navigate("./");
+      navigate('./')
     }
-  }, []);
+  }, [connector])
 
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
-      <Context.Provider
-        value={{
-          loggedIn,
-          setLoggedIn,
-          signer,
-          setSigner,
-        }}
-      >
-        <div className="wrapper__background">
-          <div className="wrapper">
-            <Routes>
-              <Route path="/" element={<LandingPage />} />
-              <Route path="/deposit-page" element={<DepositPage />} />
-              <Route path="/landing-page" element={<LandingPage />} />
-              <Route path="/lotto-page" element={<LottoPage />} />
-              <Route path="/account-page" element={<AccountPage />} />
-            </Routes>
+      <MetamaskProvider>
+        <Context.Provider
+          value={{
+            loggedIn,
+            setLoggedIn,
+            signer,
+            setSigner,
+          }}
+        >
+          <div className="wrapper__background">
+            <div className="wrapper">
+              <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/deposit-page" element={<DepositPage />} />
+                <Route path="/landing-page" element={<LandingPage />} />
+                <Route path="/lotto-page" element={<LottoPage />} />
+                <Route path="/account-page" element={<AccountPage />} />
+              </Routes>
+            </div>
           </div>
-        </div>
-      </Context.Provider>
+        </Context.Provider>
+      </MetamaskProvider>
     </Web3ReactProvider>
-  );
-};
+  )
+}
 
-export default App;
+export default App
