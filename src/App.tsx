8b40245d0735c48abaaf787a99407b64e42abeee
Arthur Zadneprovskiy <hitsuga2000@gmail.com>
2022-10-27 15:55:09 +0200

    fix: account-page footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,34 @@
-import React, { FC, useContext, useEffect } from 'react'
-import { Route, Routes, useNavigate } from 'react-router-dom'
-import { useWeb3React, Web3ReactProvider } from '@web3-react/core'
-import { injected } from './helpers/connectors'
-import { Web3Provider } from '@ethersproject/providers'
+import React, { FC, useContext, useEffect } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { useWeb3React, Web3ReactProvider } from "@web3-react/core";
+import { injected } from "./helpers/connectors";
+import { Web3Provider } from "@ethersproject/providers";
 
-import Main from './pages/MainPage/Main'
-import DepositPage from './pages/DepositPage/DepositPage'
-import LandingPage from './pages/LandingPage/LandingPage'
-import LottoPage from './pages/LottoPage/LottoPage'
-import AccountPage from './pages/AccountPage/AccountPage'
+import Main from "./pages/MainPage/Main";
+import DepositPage from "./pages/DepositPage/DepositPage";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import LottoPage from "./pages/LottoPage/LottoPage";
+import AccountPage from "./pages/AccountPage/AccountPage";
 
-import { Context } from './Context'
+import { Context } from "./Context";
 
-import './index.scss'
-import MetamaskProvider from './components/MetamaskProvider/MetamaskProvider'
+import "./index.scss";
+import MetamaskProvider from "./components/MetamaskProvider/MetamaskProvider";
 
 const App: FC = () => {
   function getLibrary(provider: any) {
     // @ts-ignore
-    return new Web3Provider(provider)
+    return new Web3Provider(provider);
   }
-  const { loggedIn, setLoggedIn, signer, setSigner } = useContext<any>(Context)
-  const { connector } = useWeb3React()
-  const navigate = useNavigate()
+  const { loggedIn, setLoggedIn, signer, setSigner } = useContext<any>(Context);
+  const { connector } = useWeb3React();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (!connector) {
-      navigate('./')
+      // navigate('./')
     }
-  }, [connector])
+  }, [connector]);
 
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
@@ -55,7 +55,7 @@ const App: FC = () => {
         </Context.Provider>
       </MetamaskProvider>
     </Web3ReactProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
