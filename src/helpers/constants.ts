a2b2c61ef9a71f20c5d3184d98f7b33d918ee8ff
sowell-owen <karakulin.toshik@gmail.com>
2022-10-28 17:57:12 +0300

    feat: new constants

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,2 +1,2 @@
-export const XENADDRESS = "0x82Fbc13cB7e1046ff9F878E7ddcF1c5190416113";
-export const LOTTERYADDRESS = "0x4059eCc92a306721F62859d03554353Ba7d468AD"; // temp, need to change before demo
+export const XENADDRESS = "0x1718C874F80204EEB7a82dcdDACA8433203c9001"; // BSC testnet
+export const LOTTERYADDRESS = "0x134b490Dff5CCc47b69ed0D1A556CCC1e84fb26A"; // BSC testnet
