82ecc10bc40e815b86e09880e806bf69b1155849
sowell-owen <karakulin.toshik@gmail.com>
2022-11-08 18:18:01 +0200

    build: new abi, contract addr

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,3 +1,3 @@
 export const XENADDRESS = "0xffa85b03EFfFCE7519CCA24CDd69ea802B616B07"; // BSC testnet
-export const LOTTERYADDRESS = "0x0A95f29867a3c4fb2aD5f35796d503dd56b50eB1"; // BSC testnet
-export const BSC_RPC_URL = `https://data-seed-prebsc-1-s1.binance.org:8545/`
+export const LOTTERYADDRESS = "0x612b60C951Aebe71bCA51941A3a9aCBf949B0133"; // BSC testnet
+export const BSC_RPC_URL = `https://data-seed-prebsc-1-s1.binance.org:8545/`;
